21f2bed004f99af8e682bc1d2fc5642721dbf0ab
5t3g0n! <ja.opazoy@gmail.com>
2022-10-25 16:22:57 +0200

    Test 2.0

--- test.py
+++ test.py
@@ -21,23 +21,21 @@ def forward(img_no):
  
     # This is for clearing the screen so that
     # our next image can pop up
-    label = Label(image=List_images[img_no-1])
+    label = Label(image=List_images[img_no])
  
     # as the list starts from 0 so we are
     # subtracting one
     label.grid(row=1, column=0, columnspan=3)
-    button_for = Button(root, text="forward",
-                        command=lambda: forward(img_no+1))
+    button_for = Button(root, text="Forward", command=lambda: forward(img_no+1))
+    print(img_no)
  
     # img_no+1 as we want the next image to pop up
-    if img_no == 4:
-        button_forward = Button(root, text="Forward",
-                                state=DISABLED)
- 
+    if img_no == 3:
+        button_forward = Button(root, text="Forward", state=DISABLED)
+  
     # img_no-1 as we want previous image when we click
     # back button
-    button_back = Button(root, text="Back",
-                         command=lambda: back(img_no-1))
+    button_back = Button(root, text="Back", command=lambda: back(img_no-1))
  
     # Placing the button in new grid
     button_back.grid(row=5, column=0)
@@ -57,12 +55,10 @@ def back(img_no):
  
     # for clearing the image for new image to pop up
     label = Label(image=List_images[img_no - 1])
-    label.grid(row=1, column=0, columnspan=3)
-    button_forward = Button(root, text="forward",
-                            command=lambda: forward(img_no + 1))
-    button_back = Button(root, text="Back",
-                         command=lambda: back(img_no - 1)) 
-                         
+    label.grid(row=1, column = 0, columnspan = 3)
+    button_forward = Button(root, text="Forward", command=lambda: forward(img_no + 1))
+    button_back = Button(root, text="Back", command=lambda: back(img_no - 1)) 
+
     # whenever the first image will be there we will
     # have the back button disabled
     if img_no == 1:
@@ -82,7 +78,7 @@ root.title("Image Viewer")
  
 # The geometry of the box which will be displayed
 # on the screen
-root.geometry("840x680")
+root.geometry("")
  
 # Adding the images using the pillow module which
 # has a class ImageTk We can directly add the
@@ -102,15 +98,12 @@ label = Label(image=image_no_1)
 label.grid(row=1, column=0, columnspan=3)
  
 # We will have three button back ,forward and exit
-button_back = Button(root, text="Back", command=back,
-                     state=DISABLED)
+button_back = Button(root, text="Back", command=back, state=DISABLED)
  
 # root.quit for closing the app
-button_exit = Button(root, text="Exit",
-                     command=root.quit)
+button_exit = Button(root, text="Exit", command=root.quit)
  
-button_forward = Button(root, text="Forward",
-                        command=lambda: forward(1))
+button_forward = Button(root, text="Forward", command=lambda: forward(1))
  
 # grid function is for placing the buttons in the frame
 button_back.grid(row=5, column=0)
