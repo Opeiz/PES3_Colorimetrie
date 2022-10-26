78628117e347bdd7e7cba276373efdefa87ea386
5t3g0n! <ja.opazoy@gmail.com>
2022-10-26 20:42:30 +0200

    Avanzando con Test

--- test.py
+++ test.py
@@ -1,113 +1,68 @@
-
-# importing the tkinter module and PIL
-# that is pillow module
 from tkinter import *
-from PIL import ImageTk, Image
-import os
-import sys
+import cv2
 
- 
- 
+w,h = 640,480
+print(h,w)
+
+def callimage(num):
+
+    path = "frames/frame" + str(num) + ".jpg"
+    frame = cv2.imread(path)
+    frame = cv2.resize(frame,(w,h))
+    cv2.imshow("test",frame) 
+
 def forward(img_no):
- 
-    # GLobal variable so that we can have
-    # access and change the variable
-    # whenever needed
+
     global label
     global button_forward
     global button_back
     global button_exit
     label.grid_forget()
- 
-    # This is for clearing the screen so that
-    # our next image can pop up
-    label = Label(image=List_images[img_no])
- 
-    # as the list starts from 0 so we are
-    # subtracting one
-    label.grid(row=1, column=0, columnspan=3)
+
+    label = Label(image=callimage(img_no))
+
     button_for = Button(root, text="Forward", command=lambda: forward(img_no+1))
-    print(img_no)
- 
-    # img_no+1 as we want the next image to pop up
-    if img_no == 3:
-        button_forward = Button(root, text="Forward", state=DISABLED)
-  
-    # img_no-1 as we want previous image when we click
-    # back button
     button_back = Button(root, text="Back", command=lambda: back(img_no-1))
- 
-    # Placing the button in new grid
-    button_back.grid(row=5, column=0)
-    button_exit.grid(row=5, column=1)
-    button_for.grid(row=5, column=2)
- 
- 
+
+    # button_back.pack()
+    # button_exit.pack()
+    # button_for.pack()
+
+
 def back(img_no):
- 
-    # We will have global variable to access these
-    # variable and change whenever needed
+
     global label
     global button_forward
     global button_back
     global button_exit
     label.grid_forget()
- 
-    # for clearing the image for new image to pop up
-    label = Label(image=List_images[img_no - 1])
-    label.grid(row=1, column = 0, columnspan = 3)
-    button_forward = Button(root, text="Forward", command=lambda: forward(img_no + 1))
+
+    label = Label(image=callimage(img_no))
+
+    button_for = Button(root, text="Forward", command=lambda: forward(img_no + 1))
     button_back = Button(root, text="Back", command=lambda: back(img_no - 1)) 
 
-    # whenever the first image will be there we will
-    # have the back button disabled
-    if img_no == 1:
-        button_back = Button(root, Text="Back", state=DISABLED)
- 
-    label.grid(row=1, column=0, columnspan=3)
-    button_back.grid(row=5, column=0)
-    button_exit.grid(row=5, column=1)
-    button_forward.grid(row=5, column=2)
- 
- 
-# Calling the Tk (The initial constructor of tkinter)
-root = Tk()
- 
-# We will make the title of our app as Image Viewer
-root.title("Image Viewer")
- 
-# The geometry of the box which will be displayed
-# on the screen
-root.geometry("")
- 
-# Adding the images using the pillow module which
-# has a class ImageTk We can directly add the
-# photos in the tkinter folder or we have to
-# give a proper path for the images
-image_no_1 = ImageTk.PhotoImage(Image.open("frames/frame0.jpg").resize((640,480)))
-image_no_2 = ImageTk.PhotoImage(Image.open("frames/frame1.jpg").resize((640,480)))
-image_no_3 = ImageTk.PhotoImage(Image.open("frames/frame2.jpg").resize((640,480)))
-image_no_4 = ImageTk.PhotoImage(Image.open("frames/frame3.jpg").resize((640,480)))
- 
-# List of the images so that we traverse the list
-List_images = [image_no_1, image_no_2, image_no_3, image_no_4]
- 
-label = Label(image=image_no_1)
- 
-# We have to show the box so this below line is needed
-label.grid(row=1, column=0, columnspan=3)
- 
-# We will have three button back ,forward and exit
-button_back = Button(root, text="Back", command=back, state=DISABLED)
- 
-# root.quit for closing the app
-button_exit = Button(root, text="Exit", command=root.quit)
- 
-button_forward = Button(root, text="Forward", command=lambda: forward(1))
- 
-# grid function is for placing the buttons in the frame
-button_back.grid(row=5, column=0)
-button_exit.grid(row=5, column=1)
-button_forward.grid(row=5, column=2)
- 
+    # button_back.pack()
+    # button_exit.pack()
+    # button_for.pack()
+
+
+root =Tk()
+
+root.title("Visionneuse Numérique")
+
+root.geometry("500x500")
+
+label = Label(image=callimage(0))
+
+button_back = Button(root, text="Back", bd = "5",command=back)
+
+button_exit = Button(root, text="Exit",bd = "5", command=root.quit)
+
+button_forward = Button(root, text="Forward",bd = "5", command=lambda: forward(1))
+
+button_back.pack()
+button_exit.pack()
+button_forward.pack()
+
 root.mainloop()
