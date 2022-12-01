12c39d9a12a3bbf28a918170d2d88f89470814be
5t3g0n! <ja.opazoy@gmail.com>
2022-12-01 10:59:00 +0100

    It's working

--- framePlayer.py
+++ framePlayer.py
@@ -51,7 +51,7 @@ def forward(img_no):
     global number
     number = img_no
 
-    callimage(img_no)
+    refresh(img_no)
 
     button_for = Button(root, text="Forward", command= lambda: forward(img_no+1))
     button_back = Button(root, text="Back", command= lambda: back(img_no-1))
@@ -66,7 +66,7 @@ def back(img_no):
     global number
     number = img_no
 
-    callimage(img_no)
+    refresh(img_no)
 
     button_for = Button(root, text="Forward", command=lambda: forward(img_no+1))
     button_back = Button(root, text="Back", command=lambda: back(img_no-1))
@@ -75,21 +75,29 @@ def back(img_no):
     button_for.grid(row=3, column=2)
 
 def show_values():
-    print (SliderR.get(), SliderG.get(), SliderB.get())
+    return (SliderR.get(), SliderG.get(), SliderB.get())
 
-def refresh(num,algo):
+def refresh(num):
 
-    algo = algo
-    print(algo)
+    ValueR, ValueG, ValueB = show_values()
+
     frame = callimage(num)
+    h,w = frame.shape[0],frame.shape[1]
 
-    b,g,r = cv2.split(frame)
-
-    # zeros = np.zeros(frame.shape[:2], dtype="uint8")
-    # cv2.imshow("B",cv2.merge([zeros, zeros, r]))
-    # cv2.imshow("G",cv2.merge([zeros, g, zeros]))
-    # cv2.imshow("R",cv2.merge([b, zeros, zeros]))
+    for x in range(0,h):
+        for y in range (0,w):
+            val = frame[x,y]
+            val[0] = val[0] + ValueB if val[0] + ValueB < 255 else 255 if val[0] + ValueB > 0 else 0 
+            val[1] = val[1] + ValueG if val[1] + ValueG < 255 else 255 if val[1] + ValueG > 0 else 0
+            val[2] = val[2] + ValueR if val[2] + ValueR < 255 else 255 if val[2] + ValueR > 0 else 0
+            frame[x,y] = val
+        
+    cv2.imshow("Visionneuse",frame) 
     
+def reset_Slider():
+    SliderB.set(0)
+    SliderG.set(0)
+    SliderR.set(0)
 
 root =Tk()
 
@@ -104,28 +112,30 @@ callimage(number)
 # Buttons
 button_back = Button(root, text="Back",command=back, state= DISABLED).grid(row=3, column=0,padx=30,pady=8)  
 button_forward = Button(root, text="Forward", command=lambda: forward(number+1)).grid(row=3, column=2)
-button_refesh = Button(root, text="Refresh", command=lambda: refresh(number,algo=2)).grid(row=3, column=1)
+button_refesh = Button(root, text="Refresh", command=lambda: refresh(number)).grid(row=3, column=1)
 button_exit = Button(root, text="Exit", command=root.quit).grid(row=4, column=1,padx=30,pady=8)
-button_show = Button(root, text="Show", command=show_values).grid(row=4, column=0,padx=30,pady=8)
+# button_show = Button(root, text="Show", command=show_values).grid(row=4, column=0,padx=30,pady=8)
 
 red_label = Label(root, text="Red").grid(row=0, column=0)
-SliderR = Scale(root, from_=0 , to=100, orient=HORIZONTAL, length=300)
+SliderR = Scale(root, from_=-255 , to=255, orient=HORIZONTAL, length=300)
 SliderR.grid(row=0, column=1, columnspan=2)
 
 green_label = Label(root, text="Green").grid(row=1, column=0)
-SliderG = Scale(root, from_=0 , to=100, orient=HORIZONTAL, length=300)
+SliderG = Scale(root, from_=-255 , to=255, orient=HORIZONTAL, length=300)
 SliderG.grid(row=1, column=1, columnspan=2)
 
 blue_label = Label(root, text="Blue").grid(row=2, column=0)
-SliderB = Scale(root, from_=0 , to=100, orient=HORIZONTAL, length=300)
+SliderB = Scale(root, from_=-255 , to=255, orient=HORIZONTAL, length=300)
 SliderB.grid(row=2, column=1, columnspan=2)
 
-alpha_label = Label(root, text="Alpha").grid(row=5, column=0)
-alpha = Scale(root, from_=-255 , to=255, orient=HORIZONTAL, length=300)
-alpha.grid(row=5, column=1, columnspan=2)
+button_reset = Button(root, text="Reset", command=lambda: reset_Slider()).grid(row=4, column=0,padx=30,pady=8)
 
-cont_label = Label(root, text="Contrast").grid(row=6, column=0)
-cont = Scale(root, from_=-127 , to=127, orient=HORIZONTAL, length=300)
-cont.grid(row=6, column=1, columnspan=2)
+# alpha_label = Label(root, text="Alpha").grid(row=5, column=0)
+# alpha = Scale(root, from_=-255 , to=255, orient=HORIZONTAL, length=300)
+# alpha.grid(row=5, column=1, columnspan=2)
+
+# cont_label = Label(root, text="Contrast").grid(row=6, column=0)
+# cont = Scale(root, from_=-127 , to=127, orient=HORIZONTAL, length=300)
+# cont.grid(row=6, column=1, columnspan=2)
 
 root.mainloop() 
