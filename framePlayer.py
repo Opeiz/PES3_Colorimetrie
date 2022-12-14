6528d6d79c21feaeb80f2f84f97ad7b01be79dd0
5t3g0n! <ja.opazoy@gmail.com>
2022-12-14 16:09:01 +0100

    Add modernism

--- framePlayer.py
+++ framePlayer.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import customtkinter as ct
 import cv2
 import numpy as np
 
@@ -8,7 +9,7 @@ def callimage(num):
 
     path = "colors/frame" + str(num) + ".jpg"
     frame = cv2.imread(path)
-    frame = cv2.resize(frame,(int(frame.shape[1]*0.4),int(frame.shape[0]*0.4)))
+    frame = cv2.resize(frame,(int(frame.shape[1]*0.5),int(frame.shape[0]*0.5)))
 
     cv2.imshow("Visionneuse",frame) 
     return frame
@@ -22,8 +23,8 @@ def forward(img_no):
 
     refresh(img_no)
 
-    button_for = Button(root, text="Forward", command= lambda: forward(img_no+1))
-    button_back = Button(root, text="Back", command= lambda: back(img_no-1))
+    button_for = ct.CTkButton(master=root, text="Forward", command= lambda: forward(img_no+1))
+    button_back = ct.CTkButton(master=root, text="Back", command= lambda: back(img_no-1))
     
     button_back.grid(row=3, column=0)
     button_for.grid(row=3, column=2)
@@ -37,8 +38,8 @@ def back(img_no):
 
     refresh(img_no)
 
-    button_for = Button(root, text="Forward", command=lambda: forward(img_no+1))
-    button_back = Button(root, text="Back", command=lambda: back(img_no-1))
+    button_for = ct.CTkButton(master=root, text="Forward", command=lambda: forward(img_no+1))
+    button_back = ct.CTkButton(master=root, text="Back", command=lambda: back(img_no-1))
 
     button_back.grid(row=3, column=0)
     button_for.grid(row=3, column=2)
@@ -64,7 +65,7 @@ def reset_Slider():
     SliderG.set(0)
     SliderR.set(0)
 
-root =Tk()
+root =ct.CTk()
 
 root.title("Visionneuse Numérique")
 
@@ -75,23 +76,23 @@ number = 0
 callimage(number)
 
 # Buttons
-button_back = Button(root, text="Back",command=back, state= DISABLED).grid(row=3, column=0,padx=30,pady=8)  
-button_forward = Button(root, text="Forward", command=lambda: forward(number+1)).grid(row=3, column=2)
-button_refesh = Button(root, text="Refresh", command=lambda: refresh(number)).grid(row=3, column=1)
-button_exit = Button(root, text="Exit", command=root.quit).grid(row=4, column=1,padx=30,pady=8)
+button_back = ct.CTkButton(master=root, text="Back",command=back, state= DISABLED).grid(row=3, column=0,padx=30,pady=8)  
+button_forward = ct.CTkButton(master=root, text="Forward", command=lambda: forward(number+1)).grid(row=3, column=2,padx=30,pady=8)
+button_refesh = ct.CTkButton(master=root, text="Refresh", command=lambda: refresh(number)).grid(row=3, column=1,padx=30,pady=8)
+button_exit = ct.CTkButton(master=root, text="Exit", command=root.quit).grid(row=4, column=1,padx=30,pady=8)
 
-red_label = Label(root, text="Red").grid(row=0, column=0)
-SliderR = Scale(root, from_=-255 , to=255, orient=HORIZONTAL, length=300)
+red_label = ct.CTkLabel(master=root, text="Red").grid(row=0, column=0)
+SliderR = ct.CTkSlider(master=root, from_=-255 , to=255, hover=True)
 SliderR.grid(row=0, column=1, columnspan=2)
 
-green_label = Label(root, text="Green").grid(row=1, column=0)
-SliderG = Scale(root, from_=-255 , to=255, orient=HORIZONTAL, length=300)
+green_label = ct.CTkLabel(master=root, text="Green").grid(row=1, column=0)
+SliderG = ct.CTkSlider(master=root, from_=-255 , to=255)
 SliderG.grid(row=1, column=1, columnspan=2)
 
-blue_label = Label(root, text="Blue").grid(row=2, column=0)
-SliderB = Scale(root, from_=-255 , to=255, orient=HORIZONTAL, length=300)
+blue_label = ct.CTkLabel(master=root, text="Blue").grid(row=2, column=0)
+SliderB = ct.CTkSlider(master=root, from_=-255 , to=255)
 SliderB.grid(row=2, column=1, columnspan=2)
 
-button_reset = Button(root, text="Reset", command=lambda: reset_Slider()).grid(row=4, column=0,padx=30,pady=8)
+button_reset = ct.CTkButton(master=root, text="Reset", command=lambda: reset_Slider()).grid(row=4, column=0,padx=30,pady=8)
 
 root.mainloop()
